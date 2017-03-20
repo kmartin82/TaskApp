b89d71ed1d04cd12b91a0fdada670bba4ed254e1
sarah-mcculley <smcculle@gmail.com>
2017-03-20 16:27:35 -0400

    created SingleFragmentActivity and updated TaskActivity

---
+++ app/src/main/java/com/firstandroidclass/taskapp/SingleFragmentActivity.java
@@ -0,0 +1,33 @@
+package com.firstandroidclass.taskapp;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+/**
+ * Created by sarahmcculley on 3/20/17.
+ */
+
+public abstract class SingleFragmentActivity extends FragmentActivity {
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment =fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+
+
+}
